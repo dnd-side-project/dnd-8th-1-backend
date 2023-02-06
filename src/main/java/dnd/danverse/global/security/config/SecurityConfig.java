47d0aeedbdb772f57260d92fb3afc89267e99285
soochan <tncksdl05@gmail.com>
2023-02-06 13:23:20 +0900

    feat : security config 관련 코드 추가 (#12)

---
+++ src/main/java/dnd/danverse/global/security/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package dnd.danverse.global.security.config;
+
+import dnd.danverse.domain.jwt.handler.JwtAuthenticationEntryPointHandler;
+import dnd.danverse.domain.jwt.filter.JwtAuthenticationFilter;
+import dnd.danverse.domain.jwt.exception.JwtExceptionFilter;
+import dnd.danverse.domain.jwt.handler.JwtAccessDeniedHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Spring Security 설정
+ * 권한이 필요한 요청에 대해서 정의할 수 있게 한다.
+ */
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  private final JwtAuthenticationEntryPointHandler jwtAuthenticationEntryPointHandler;
+  private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+  private final JwtExceptionFilter jwtExceptionFilter;
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.cors()
+        .and()
+        .csrf().disable()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    http.exceptionHandling()
+        .authenticationEntryPoint(jwtAuthenticationEntryPointHandler) // 인증 되지 않는 사용자가 접근할 경우
+        .accessDeniedHandler(jwtAccessDeniedHandler); // 인증은 했으나 권한이 없는 경우
+
+
+
+    http.authorizeHttpRequests()
+        .antMatchers("/api/member/oauth/google/login","/api/jwt/refresh").permitAll()
+        .antMatchers("/api/manager/resource").hasAuthority("ROLE_USER_PROFILE_YES")
+        .anyRequest().authenticated();
+
+    http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    http.addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class);
+
+
+    return http.build();
+  }
+}
+
