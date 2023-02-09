ba5417772e20386866042b82d00b3b95c0ff54a9
soochan <tncksdl05@gmail.com>
2023-02-09 16:50:51 +0900

    refactor : redis 에 저장되는 객체 필드 수정 (#36)
    - key 값을 email 로 설정
    - value 값을 refresh token 으로 설정
    - refresh token 은 index 로 관리 대상이기에 조회 가능

--- src/main/java/dnd/danverse/global/redis/dto/RefreshTokenDto.java
+++ src/main/java/dnd/danverse/global/redis/dto/RefreshTokenDto.java
@@ -8,11 +8,12 @@ import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 /**
  * Redis 에 저장되는 객체.
  * RefreshToken 은 1주일의 유효시간을 가진다.
- * key = refreshToken , value = email
+ * key = email , value = refreshToken
  * value 에 Indexed 를 생성 함으로써, value 를 통해 조회가 가능하다.
  */
 @Getter
@@ -22,9 +23,10 @@ import org.springframework.data.redis.core.RedisHash;
 public class RefreshTokenDto {
 
   @Id
-  private String refreshToken;
-
   private String email;
+
+  @Indexed
+  private String refreshToken;
 
 }
 
