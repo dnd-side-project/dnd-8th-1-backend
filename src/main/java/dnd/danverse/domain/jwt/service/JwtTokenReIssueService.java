df3fd8c71dbc7fb26ae459b896b4f638df827db1
soochan <tncksdl05@gmail.com>
2023-02-09 16:53:53 +0900

    chore : 메서드 네이밍 수정  (#36)

--- src/main/java/dnd/danverse/domain/jwt/service/JwtTokenReIssueService.java
+++ src/main/java/dnd/danverse/domain/jwt/service/JwtTokenReIssueService.java
@@ -19,20 +19,25 @@ public class JwtTokenReIssueService {
   private final JwtTokenProvider jwtTokenProvider;
   private final RedisService redisService;
 
+  /**
+   * Refresh Token 을 이용하여 Access Token 과 Refresh Token 을 재발급 받는다.
+   * @param refreshToken : Client 로 넘어온 Refresh Token
+   * @return : Access Token 과 Refresh Token 을 담은 AccessRefreshTokenDto
+   */
   public AccessRefreshTokenDto reIssueToken(String refreshToken) {
 
     jwtTokenProvider.validateToken(refreshToken);
 
-    Optional<RefreshTokenDto> tokenDto = redisService.isExist(refreshToken);
+    Optional<RefreshTokenDto> tokenDto = redisService.isRefreshTokenExist(refreshToken);
 
-    String newAccessToken = null;
-    String newRefreshToken = null;
+    String newAccessToken;
+    String newRefreshToken;
     // Refresh Token 이 존재하면, 새로운 Access Token 과 Refresh Token 을 발급한다.
     if (tokenDto.isPresent()) {
       String email = tokenDto.get().getEmail();
       newAccessToken = jwtTokenProvider.createAccessToken(email);
       newRefreshToken = jwtTokenProvider.createRefreshToken();
-      redisService.saveRefreshToken(newRefreshToken, email);
+      redisService.saveRefreshToken(email, newRefreshToken);
     } else {
       // Refresh Token 이 존재하지 않으면, Refresh Token 이 Redis 에서도 만료 됨을 의미, 그 만큼(1주일) 오랜 기간 접속하지 않았다.
       throw new IllegalArgumentException("Refresh Token 이 존재하지 않습니다. 새롭게 로그인 해주세요");
