be87661a71f48aad5988243268822d379e6e7771
soochan <tncksdl05@gmail.com>
2023-02-11 13:41:27 +0900

    feat : 공연 테스트에 필요한 repository 추가 (#43)

---
+++ src/main/java/dnd/danverse/domain/profile/ProfileRepository.java
@@ -0,0 +1,10 @@
+package dnd.danverse.domain.profile;
+
+import dnd.danverse.domain.profile.entity.Profile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
+
+}
