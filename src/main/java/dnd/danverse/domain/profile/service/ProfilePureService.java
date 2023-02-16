0775bd728df9296de1991550f846610aeaf17fed
soochan <tncksdl05@gmail.com>
2023-02-16 18:59:23 +0900

    chore : 주석 변경 (#63)

--- src/main/java/dnd/danverse/domain/profile/service/ProfilePureService.java
+++ src/main/java/dnd/danverse/domain/profile/service/ProfilePureService.java
@@ -21,7 +21,7 @@ public class ProfilePureService {
   private final ProfileRepository profileRepository;
 
   /**
-   * 프로필 조회
+   * member Id 를 통해서 프로필 조회
    * @param memberId 사용자 (member 고유 DB ID)
    * @return 프로필
    */
