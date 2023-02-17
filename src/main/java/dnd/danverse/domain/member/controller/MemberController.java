93afef103c8bb3ffa158253e88a6a7ec3548dd95
leeseolhui <lsh81550@gmail.com>
2023-02-17 23:56:34 +0900

    setting : member 관련 스웨거 매핑 추가 (#71)

--- src/main/java/dnd/danverse/domain/member/controller/MemberController.java
+++ src/main/java/dnd/danverse/domain/member/controller/MemberController.java
@@ -8,6 +8,9 @@ import dnd.danverse.domain.oauth.service.OAuth2Service;
 import dnd.danverse.global.response.DataResponse;
 import dnd.danverse.global.util.CookieUtil;
 import dnd.danverse.global.util.HttpHeaderUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -19,11 +22,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * 회원 관련 컨트롤러
+ * 회원 관련 컨트롤러.
  */
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/member")
+@Api(tags = "MemberController : 멤버 관련 API")
 public class MemberController {
 
   private final OAuth2Service oAuth2Service;
@@ -31,9 +35,12 @@ public class MemberController {
   /**
    * 클라이언트 Request 으로부터 Header 에 있는 Authorization 에 담긴 토큰을 받아서
    * 회원 가입을 하고, JWT 토큰을 생성하여 반환한다.
+   *
    * @return Header 에 Access Token , Cookie 에 Refresh Token 을 담아서 반환한다.
    */
   @GetMapping(value = "/oauth/google/login")
+  @ApiOperation(value = "구글 연동 로그인을 통한 회원가입", notes = "발급받은 구글 토큰으로 회원가입 혹은 로그인을 할 수 있습니다.")
+  @ApiImplicitParam(name = "google-token", value = "구글 서비스로부터 발급받은 토큰(header의 key는 google-token 입니다)", required = true, dataType = "string", paramType = "header")
   public ResponseEntity<DataResponse<MemberResponse>> oauth2Login(@RequestHeader("google-token") String googleToken) {
     OAuth2LoginResponseDTO responseDto = oAuth2Service.oauth2Login(googleToken);
 
--- src/main/java/dnd/danverse/domain/profile/dto/response/ProfileInfoDto.java
+++ src/main/java/dnd/danverse/domain/profile/dto/response/ProfileInfoDto.java
@@ -2,11 +2,12 @@ package dnd.danverse.domain.profile.dto.response;
 
 import dnd.danverse.domain.profile.entity.Portfolio;
 import dnd.danverse.domain.profile.entity.Profile;
+import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDate;
 import lombok.Getter;
 
 /**
- * 프로필 정보를 반환하기 위한 Dto
+ * 프로필 정보를 반환하기 위한 Dto.
  */
 @Getter
 public class ProfileInfoDto {
@@ -14,57 +15,79 @@ public class ProfileInfoDto {
   /**
    * 프로필의 고유 ID.
    */
+  @ApiModelProperty(value = "프로필의 고유 ID")
   private final Long id;
+
   /**
    * 프로필의 타입. (팀, 개인)
    */
+  @ApiModelProperty(value = "프로필의 타입 (팀, 개인)")
   private final String type;
+
   /**
    * 프로필의 이름. (팀명, 개인명)
    */
+  @ApiModelProperty(value = "프로필의 이름 (팀명, 개인명)")
   private final String name;
+
   /**
    * 프로필의 이미지 URL.
    */
+  @ApiModelProperty(value = "프로필의 이미지 URL")
   private final String imgUrl;
+
   /**
-   * 프로필의 작성자의 주 활동 지역
+   * 프로필의 작성자의 주 활동 지역.
    */
+  @ApiModelProperty(value = "프로필의 작성자의 주 활동 지역")
   private final String location;
+
   /**
-   * 프로필의 작성자의 활동 시작일
+   * 프로필의 작성자의 활동 시작일.
    */
+  @ApiModelProperty(value = "프로필의 작성자의 활동 시작일")
   private final LocalDate careerStartDay;
+
   /**
-   * 프로필의 작성자의 자기소개
+   * 프로필의 작성자의 자기소개.
    */
+  @ApiModelProperty(value = "프로필의 작성자의 자기소개")
   private final String description;
+
   /**
-   * 프로필의 작성자의 카카오톡 오픈채팅 URL
+   * 프로필의 작성자의 카카오톡 오픈채팅 URL.
    */
+  @ApiModelProperty(value = "프로필의 작성자의 카카오톡 오픈채팅 URL")
   private final String openChatUrl;
+
   /**
-   * 프로필의 작성자의 포트폴리오 URL Dto
+   * 프로필의 작성자의 포트폴리오 URL Dto.
    */
+  @ApiModelProperty(value = "프로필의 포트폴리오 정보")
   private final PortfolioUrl portfolioUrl;
 
   /**
-   * 프로필의 작성자의 포트폴리오 URL Dto
+   * 프로필의 작성자의 포트폴리오 URL Dto.
    */
   @Getter
   public static class PortfolioUrl {
 
     /**
-     * 프로필의 작성자의 유튜브 URL
+     * 프로필의 작성자의 유튜브 URL.
      */
+    @ApiModelProperty(value = "포트폴리오의 유튜브 URL")
     private final String youtube;
+
     /**
-     * 프로필의 작성자의 인스타그램 URL
+     * 프로필의 작성자의 인스타그램 URL.
      */
+    @ApiModelProperty(value = "포트폴리오의 인스타 URL")
     private final String instagram;
+
     /**
-     * 프로필의 작성자의 트위터 URL
+     * 프로필의 작성자의 트위터 URL.
      */
+    @ApiModelProperty(value = "포트폴리오의 트위터 URL")
     private final String twitter;
 
     public PortfolioUrl(Portfolio portfolio) {
@@ -76,6 +99,7 @@ public class ProfileInfoDto {
 
   /**
    * Profile 객체를 이용하여 ProfileInfoDto 를 생성한다.
+   *
    * @param profile 프로필 정보를 담은 객체
    */
   public ProfileInfoDto(Profile profile) {
