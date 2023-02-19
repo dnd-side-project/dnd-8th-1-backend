632a2c941a348dd2afdb047d04edcfe3cc0ee1e1
leeseolhui <lsh81550@gmail.com>
2023-02-19 14:11:09 +0900

    feat : 공연 정보글 상세조회 기능 구현 (#83)

---
+++ src/main/java/dnd/danverse/domain/performance/service/PerformSearchComplexService.java
@@ -0,0 +1,24 @@
+package dnd.danverse.domain.performance.service;
+
+import dnd.danverse.domain.performance.dto.response.PerformDetailResponse;
+import dnd.danverse.domain.performance.entity.Performance;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class PerformSearchComplexService {
+  private final PerformancePureService performancePureService;
+
+  /**
+   * 공연 정보 상세 조회.
+   *
+   * @param performId 조회하고자 하는 performId.
+   * @return 공연 정보와 프로필 정보를 담은 responseDto 반환.
+   */
+  public PerformDetailResponse getDetailPerform(Long performId) {
+    Performance perform = performancePureService.getPerformanceDetail(performId);
+    return new PerformDetailResponse(perform);
+  }
+}
--- src/main/java/dnd/danverse/domain/performgenre/repository/PerformGenreRepository.java
+++ src/main/java/dnd/danverse/domain/performgenre/repository/PerformGenreRepository.java
@@ -1,4 +1,4 @@
-package dnd.danverse.domain.performgenre;
+package dnd.danverse.domain.performgenre.repository;
 
 import dnd.danverse.domain.performgenre.entity.PerformGenre;
 import org.springframework.data.jpa.repository.JpaRepository;
