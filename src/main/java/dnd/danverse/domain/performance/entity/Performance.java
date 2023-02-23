c357a402c657cabfa34e1ee85fe05fe6b2ec0c5e
leeseolhui <lsh81550@gmail.com>
2023-02-23 18:18:14 +0900

    feat : 공연 정보 수정 장르 변경이 생겼을 경우의 새로운 response dto 생성자 구현 (#101)

--- src/main/java/dnd/danverse/domain/performance/entity/Performance.java
+++ src/main/java/dnd/danverse/domain/performance/entity/Performance.java
@@ -2,12 +2,16 @@ package dnd.danverse.domain.performance.entity;
 
 import dnd.danverse.domain.common.BaseTimeEntity;
 import dnd.danverse.domain.common.Image;
+import dnd.danverse.domain.performance.dto.request.PerformUpdateRequestDto;
 import dnd.danverse.domain.performgenre.entity.PerformGenre;
 import dnd.danverse.domain.profile.entity.Profile;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
@@ -64,8 +68,8 @@ public class Performance extends BaseTimeEntity {
    * 공연 장르 목록 PerformGenre 의 삭제는 Performance 가 삭제될 때 함께 삭제된다. CascadeType.PERSIST 를 하지 않고, 추후
    * saveAll()를 통해서 한번의 네트워크 통신으로 처리한다.
    */
-  @OneToMany(mappedBy = "performance", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-  private List<PerformGenre> performGenres = new ArrayList<>();
+  @OneToMany(mappedBy = "performance", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+  private Set<PerformGenre> performGenres = new HashSet<>();
 
   /**
    * 공연 제목.
@@ -127,7 +131,7 @@ public class Performance extends BaseTimeEntity {
   /**
    * 공연 장르 목록을 문자열로 반환한다.
    * 
-   * @return 장르 List
+   * @return 장르 List.
    */
   public List<String> getStringOfPerformGenres() {
     List<String> genres = new ArrayList<>();
@@ -135,6 +139,37 @@ public class Performance extends BaseTimeEntity {
       genres.add(genre.getGenre());
     }
     return genres;
+  }
+
+
+  public Performance updateInfo(PerformUpdateRequestDto updateRequest) {
+    this.title = updateRequest.getTitle();
+    this.location = updateRequest.getLocation();
+    this.address = updateRequest.getAddress();
+    this.startDate = updateRequest.getStartDate();
+    this.startTime = updateRequest.getStartTime();
+    this.performanceImg = new Image(updateRequest.getImgUrl());
+    this.description = updateRequest.getDescription();
+    return this;
+  }
+
+  /**
+   * PerformGenre -> String 으로 변환
+   */
+  private Set<String> getOriginalGenres() {
+    return this.performGenres.stream()
+        .map(PerformGenre::getGenre)
+        .collect(Collectors.toSet());
+  }
+
+  /**
+   * 수정하려는 장르의 Set 과 기존 장르의 Set 을 containsAll 메서드로 비교합니다.
+   *
+   * @param newGenres 새로운 장르 Set<String>
+   * @return 모두 포함한다면 true, 하나라도 중복되지 않는 값이 있다면 false.
+   */
+  public boolean isSame(Set<String> newGenres) {
+    return getOriginalGenres().containsAll(newGenres);
   }
 
 }
