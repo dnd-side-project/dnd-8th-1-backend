f4a21b70aed4e3b7b0bac83974c551a50e780dfa
soochan <91618389+soochangoforit@users.noreply.github.com>
2023-02-23 17:54:00 +0900

    refactor : 전체 조회 시, page size 15로 고정 (#119) (#120)

--- src/main/java/dnd/danverse/domain/performance/repository/PerformFilterCustom.java
+++ src/main/java/dnd/danverse/domain/performance/repository/PerformFilterCustom.java
@@ -5,14 +5,13 @@ import dnd.danverse.domain.performance.dto.response.PerformInfoResponse;
 import dnd.danverse.domain.performance.entity.Performance;
 import java.util.List;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 /**
  * 공연을 필터링을 통해서 조회하는 커스텀 인터페이스.
  */
 public interface PerformFilterCustom {
 
-  Page<PerformInfoResponse> searchAllPerformWithCond(PerformCondDto performCondDto, Pageable pageable);
+  Page<PerformInfoResponse> searchAllPerformWithCond(PerformCondDto performCondDto);
 
   List<Performance> searchPerformsByTeam(String teamName);
 
