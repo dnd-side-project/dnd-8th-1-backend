02b791615498113510568c75c638269e8a2d11da
leeseolhui <lsh81550@gmail.com>
2023-02-23 18:20:40 +0900

    feat : 공연 순수 서비스에서 요청 dto에 따라 수정할 수 있는 기능 구현 (#101)

--- src/main/java/dnd/danverse/domain/performance/service/PerformancePureService.java
+++ src/main/java/dnd/danverse/domain/performance/service/PerformancePureService.java
@@ -2,6 +2,7 @@ package dnd.danverse.domain.performance.service;
 
 import static dnd.danverse.global.exception.ErrorCode.PERFORMANCE_NOT_FOUND;
 
+import dnd.danverse.domain.performance.dto.request.PerformUpdateRequestDto;
 import dnd.danverse.domain.performance.dto.response.ImminentPerformsDto;
 import dnd.danverse.domain.performance.entity.Performance;
 import dnd.danverse.domain.performance.exception.PerformanceNotFoundException;
@@ -10,6 +11,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @Transactional(readOnly = true)
 public class PerformancePureService {
 
@@ -39,6 +42,7 @@ public class PerformancePureService {
 
   @Transactional(readOnly = true)
   public Performance getPerformanceDetail(Long performId) {
+    log.info("performance와 작성자의 profile을 fetch join 하여 찾습니다. performId: {}", performId);
     return performanceRepository.findPerformanceWithProfile(performId)
         .orElseThrow(() -> new PerformanceNotFoundException(PERFORMANCE_NOT_FOUND));
   }
@@ -48,5 +52,17 @@ public class PerformancePureService {
     return performanceRepository.save(performance);
   }
 
+  /**
+   * 공연 정보를 updateRequestDto 대로 수정합니다.
+   *
+   * @param performance 수정하려는 공연.
+   * @param request 수정 요청 Dto.
+   * @return 요청 Dto 대로 수정합니다.
+   */
+  @Transactional
+  public Performance updatePerform(Performance performance, PerformUpdateRequestDto request) {
+    log.info("요청 dto로 수정합니다.");
+    return performance.updateInfo(request);
+  }
 
 }
