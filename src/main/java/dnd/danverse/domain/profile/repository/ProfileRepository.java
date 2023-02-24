a2f0e144cb5c25d0f6bfbc6051ec519fa9332c8b
soochan <tncksdl05@gmail.com>
2023-02-24 12:24:58 +0900

    chore : 사용하지 않는 query 제거 (#131)

--- src/main/java/dnd/danverse/domain/profile/repository/ProfileRepository.java
+++ src/main/java/dnd/danverse/domain/profile/repository/ProfileRepository.java
@@ -3,7 +3,6 @@ package dnd.danverse.domain.profile.repository;
 import dnd.danverse.domain.profile.entity.Profile;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -21,6 +20,4 @@ public interface ProfileRepository extends JpaRepository<Profile, Long> {
   @Query("SELECT p FROM Profile p")
   List<Profile> findAllProfiles();
 
-  @Query("SELECT p FROM Profile p WHERE p.member.id IN :memberIds")
-  List<Profile> findAllProfilesWithMemberIds(@Param("memberIds") Set<Long> memberIds);
 }
