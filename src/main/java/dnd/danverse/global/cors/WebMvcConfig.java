a7f3236c6d749a0b0b88bc55538a1acdd64ef153
soochan <tncksdl05@gmail.com>
2023-02-24 15:36:30 +0900

    feat : cors 6006 포트 추가 (#137)

--- src/main/java/dnd/danverse/global/cors/WebMvcConfig.java
+++ src/main/java/dnd/danverse/global/cors/WebMvcConfig.java
@@ -16,7 +16,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry){
     registry
         .addMapping("/**")
-        .allowedOrigins("http://localhost:3000")
+        .allowedOrigins("http://localhost:3000", "http://localhost:6006")
         .allowedMethods(
             HttpMethod.GET.name(),
             HttpMethod.POST.name(),
