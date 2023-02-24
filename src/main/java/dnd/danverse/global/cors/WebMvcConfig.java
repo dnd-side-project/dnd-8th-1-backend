898a1e7d3c05ae3108c0c2477af9537ed119d840
leeseolhui <lsh81550@gmail.com>
2023-02-25 01:48:54 +0900

    feat : 프론트의 배포 url을 cors 설정 추가 (#145)

--- src/main/java/dnd/danverse/global/cors/WebMvcConfig.java
+++ src/main/java/dnd/danverse/global/cors/WebMvcConfig.java
@@ -13,10 +13,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
   @Override
-  public void addCorsMappings(CorsRegistry registry){
+  public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/**")
-        .allowedOrigins("http://localhost:3000", "http://localhost:6006")
+        .allowedOrigins("http://localhost:3000", "http://localhost:6006", "https://danverse-git-*-allsilver.vercel.app")
         .allowedMethods(
             HttpMethod.GET.name(),
             HttpMethod.POST.name(),
