7b31aa5a1e76b691685f274021912176623bb1de
soochan <91618389+soochangoforit@users.noreply.github.com>
2023-02-24 10:14:05 +0900

    refactor : 공연 삭제 시, validation 추상화에 의존하도록 변경 (#129) (#130)

--- src/main/java/dnd/danverse/domain/performance/service/PerformDeleteService.java
+++ src/main/java/dnd/danverse/domain/performance/service/PerformDeleteService.java
@@ -1,6 +1,7 @@
 package dnd.danverse.domain.performance.service;
 
 import dnd.danverse.domain.performance.entity.Performance;
+import dnd.danverse.domain.validation.WriterValidationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,19 +13,19 @@ import org.springframework.stereotype.Service;
 public class PerformDeleteService {
 
   private final PerformancePureService performancePureService;
-  private final PerformWriterValidationService performWriterValidationService;
+  private final WriterValidationService<Performance> validatePerformWriter;
 
   /**
    * 작성자와 삭제 요청 사용자가 동일한지 확인합니다.
    * 동일하다면, 삭제가 이뤄집니다.
    * 동일하지 않다면, 예외 처리를 해줍니다.
+   * 추상화 된 validationService 에 의존하고 있습니다.
    *
    * @param performId 삭제하려는 공연 고유 Id.
    * @param memberId 삭제 요청 사용자 고유 Id.
    */
   public void deletePerform(Long performId, Long memberId) {
-    performWriterValidationService.validatePerformWriter(performId, memberId);
-    Performance performance = performancePureService.getPerformance(performId);
+    Performance performance = validatePerformWriter.validateWriter(performId, memberId);
     performancePureService.deletePerform(performance);
   }
 }
