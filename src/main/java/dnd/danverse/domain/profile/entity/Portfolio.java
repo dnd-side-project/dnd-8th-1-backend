fef28d5e0051243c563449a5ede8d450711928d7
leeseolhui <lsh81550@gmail.com>
2023-02-25 23:23:20 +0900

    fix : 엔티티에서 직접적으로 dto 에 의존하는 문제 해결 (#147)

--- src/main/java/dnd/danverse/domain/profile/entity/Portfolio.java
+++ src/main/java/dnd/danverse/domain/profile/entity/Portfolio.java
@@ -1,7 +1,5 @@
 package dnd.danverse.domain.profile.entity;
 
-
-import dnd.danverse.domain.profile.dto.request.ProfileSaveRequestDto.PortfolioUrl;
 import javax.persistence.Embeddable;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,21 +16,10 @@ public class Portfolio {
   private String youtubeUrl;
   private String instagramUrl;
   private String twitterUrl;
-
+  
   public Portfolio(String youtubeUrl, String instagramUrl, String twitterUrl) {
     this.youtubeUrl = youtubeUrl;
     this.instagramUrl = instagramUrl;
     this.twitterUrl = twitterUrl;
-  }
-
-  /**
-   * 포트폴리오 정보를 담는 dto를 엔티티로 변환한다.
-   *
-   * @param portfolio 포트폴리오 정보를 담은 Dto.
-   */
-  public Portfolio(PortfolioUrl portfolio) {
-    this.youtubeUrl = portfolio.getYoutube();
-    this.instagramUrl = portfolio.getInstagram();
-    this.twitterUrl = portfolio.getTwitter();
   }
 }
