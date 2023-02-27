10e137802bd60f1904488923f1e3db282fee5043
leeseolhui <lsh81550@gmail.com>
2023-02-27 16:17:52 +0900

    feat: 리뷰 조회 순수 서비스 구현 (#169)

--- src/main/java/dnd/danverse/domain/review/service/ReviewPureService.java
+++ src/main/java/dnd/danverse/domain/review/service/ReviewPureService.java
@@ -2,6 +2,7 @@ package dnd.danverse.domain.review.service;
 
 import static dnd.danverse.global.exception.ErrorCode.REVIEW_NOT_FOUND;
 
+import dnd.danverse.domain.mypage.dto.response.MyReviewDto;
 import dnd.danverse.domain.review.dto.response.ReviewInfoDto;
 import dnd.danverse.domain.review.dto.response.ReviewInfoWithPerformDto;
 import dnd.danverse.domain.review.entity.Review;
@@ -25,7 +26,8 @@ public class ReviewPureService {
 
   /**
    * Review 를 저장한다.
-   * @param review Review
+   *
+   * @param review Review.
    * @return Review
    */
   @Transactional
@@ -40,6 +42,7 @@ public class ReviewPureService {
    * Member 와 Profile 은 left join fetch 를 시도하려 했지만,
    * 응답에 필요한 데이터에 비해, 너무 많은 컬럼을 조회하게 되어, 성능이 떨어지는 문제가 발생했다.
    * 따라서, 별로의 Dto 를 만들어, 응답에 필요한 데이터만을 조회하도록 했다.
+   *
    * @param performId 공연 ID
    * @return 공연 리뷰 리스트
    */
@@ -52,7 +55,8 @@ public class ReviewPureService {
   /**
    * 최근 리뷰를 조회한다.
    * Entity 를 그대로 응답하는 것이 아닌, 별도의 Dto 를 만들어, 응답에 필요한 데이터만을 조회하도록 했다.
-   * @return 최근 리뷰 리스트
+   * 
+   * @return 최근 리뷰 리스트.
    */
   @Transactional(readOnly = true)
   public List<ReviewInfoWithPerformDto> findRecentReviews() {
@@ -62,6 +66,7 @@ public class ReviewPureService {
   /**
    * 공연에 대한 리뷰를 조회한다
    * Review, Member, Profile 을 fetch join 한다.
+   *
    * @param reviewId 리뷰 ID
    * @return 리뷰
    */
@@ -73,7 +78,8 @@ public class ReviewPureService {
 
   /**
    * Review 를 수정 한다.
-   * @param review 수정하고자 하는 Review
+   *
+   * @param review 수정하고자 하는 Review.
    * @param reviewContent 수정하고자 하는 Review 내용
    * @return 수정된 Review
    */
@@ -81,5 +87,17 @@ public class ReviewPureService {
   public Review updateReview(Review review, String reviewContent) {
     log.info("Review 를 수정한다. 내용 : {} ", reviewContent);
     return review.updateReview(reviewContent);
+  }
+
+  /**
+   * memberId 를 통해서 작성한 모든 리뷰를 가져온다.
+   *
+   * @param memberId 멤버의 고유 Id.
+   * @return 자신이 작성한 리뷰 응답 Dto.
+   */
+  @Transactional(readOnly = true)
+  public List<MyReviewDto> findAllReviewsByMemberId(Long memberId) {
+    log.info("작성자의 아이디가 {}인 리뷰를 조회한다.", memberId);
+    return reviewRepository.findReviewsWithMemberId(memberId);
   }
 }
