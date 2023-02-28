747aacde267182094843a6db0650d9bf0626380c
soochan <tncksdl05@gmail.com>
2023-03-01 01:32:14 +0900

    refactor : cookie sameSite 옵션 변경 (#179)

--- src/main/java/dnd/danverse/global/util/CookieUtil.java
+++ src/main/java/dnd/danverse/global/util/CookieUtil.java
@@ -18,6 +18,9 @@ public class CookieUtil {
 
   /**
    * Header 에 Refresh Token Cookie 를 추가한다.
+   * sameSite 설정을 위해서 secure 속성도 함께 가져가야 한다.
+   * https://velog.io/@pjh612/JWTCORS-%EC%9D%B4%EC%8A%88-%EC%83%81%ED%99%A9%EC%97%90%EC%84%9C-%EC%BF%A0%ED%82%A4%EC%A0%84%EB%8B%AC
+   *
    * @param refreshToken 서버에서 생성한 Refresh Token
    */
   public static void setRefreshCookie(HttpHeaders httpHeaders, String refreshToken) {
@@ -27,6 +30,7 @@ public class CookieUtil {
         .secure(true)
         .maxAge(REFRESH_TOKEN_EXPIRE_LENGTH_MS)
         .path("/")
+        .sameSite("None")
         .build();
 
     httpHeaders.add(HttpHeaders.SET_COOKIE, cookie.toString());
