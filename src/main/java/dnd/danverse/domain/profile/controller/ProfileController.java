3e03628c4c48b5c39368142341001aa086b35cb1
soochan <tncksdl05@gmail.com>
2023-03-02 23:44:56 +0900

    feat : 프로필 삭제 기능 구현 (#206)

--- src/main/java/dnd/danverse/domain/profile/controller/ProfileController.java
+++ src/main/java/dnd/danverse/domain/profile/controller/ProfileController.java
@@ -6,11 +6,13 @@ import dnd.danverse.domain.profile.dto.request.ProfileUpdateRequestDto;
 import dnd.danverse.domain.profile.dto.response.ProfileDetailResponseDto;
 import dnd.danverse.domain.profile.dto.response.ProfileHomeDto;
 import dnd.danverse.domain.profile.dto.response.MemberWithProfileDto;
+import dnd.danverse.domain.profile.service.ProfileDeleteComplexService;
 import dnd.danverse.domain.profile.service.ProfileDetailService;
 import dnd.danverse.domain.profile.service.ProfileSaveService;
 import dnd.danverse.domain.profile.service.ProfileSearchService;
 import dnd.danverse.domain.profile.service.ProfileUpdateService;
 import dnd.danverse.global.response.DataResponse;
+import dnd.danverse.global.response.MessageResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -21,6 +23,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,6 +45,7 @@ public class ProfileController {
   private final ProfileDetailService profileDetailService;
   private final ProfileSaveService profileSaveService;
   private final ProfileUpdateService profileUpdateService;
+  private final ProfileDeleteComplexService profileDeleteComplexService;
 
   /**
    * 서비스 사용자는 실제 사용자의 프로필 6개를 랜덤으로 조회할 수 있습니다.
@@ -91,4 +95,21 @@ public class ProfileController {
     ProfileDetailResponseDto detailResponse = profileUpdateService.updateProfile(profileUpdateRequest, sessionUser.getId());
     return new ResponseEntity<>(DataResponse.of(HttpStatus.OK, "프로필 수정 성공", detailResponse), HttpStatus.OK);
   }
+
+  /**
+   * 프로필을 등록한 사용자는 자신의 프로필을 삭제할 수 있습니다.
+   *
+   * @param sessionUser API 호출 사용자
+   * @return 프로필 삭제 성공에 대한 메시지를 응답한다.
+   */
+  @DeleteMapping()
+  @ApiOperation(value = "프로필 삭제", notes = "자신의 프로필을 삭제 할 수 있다.")
+  @ApiImplicitParam(name = "Authorization", value = "Bearer access_token (서버에서 발급한 access_token)",
+      required = true, dataType = "string", paramType = "header")
+  public ResponseEntity<MessageResponse> deleteProfile(@AuthenticationPrincipal SessionUser sessionUser) {
+    profileDeleteComplexService.deleteProfile(sessionUser.getId());
+    return new ResponseEntity<>(MessageResponse.of(HttpStatus.OK, "프로필 삭제 성공"), HttpStatus.OK);
+  }
+
+
 }
